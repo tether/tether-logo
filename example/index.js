bdf554a51495b69ba41e01b6da6a4e83c76e4d6d
bredele <olivier.wietrich@gmail.com>
2017-06-07 13:50:12 -0600

    Create example

---
+++ example/index.js
@@ -0,0 +1,16 @@
+/**
+ * Dependencies.
+ */
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Logo from '..'
+
+/**
+ * Render badge.
+ */
+
+ReactDOM.render(
+  <Logo />,
+  document.querySelector('main')
+)
